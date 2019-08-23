2fd5d1b474006d9b64cc6e9b93046377bc1586b8
Alex <583212965@qq.com>
2019-08-23 14:22:04 +0800

    Menu

---
+++ 8 Menu/ChildView.cpp
@@ -0,0 +1,125 @@
+﻿
+// ChildView.cpp: CChildView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Menu.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView():m_nShape(0)
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_COMMAND(ID_SHAPE_CIRCLE, &CChildView::OnShapeCircle)
+	ON_COMMAND(ID_SHAPE_TRIANGLE, &CChildView::OnShapeTriangle)
+	ON_COMMAND(ID_SHAPE_SQUARE, &CChildView::OnShapeSquare)
+	ON_UPDATE_COMMAND_UI(ID_SHAPE_CIRCLE, &CChildView::OnUpdateShapeCircle)
+	ON_UPDATE_COMMAND_UI(ID_SHAPE_TRIANGLE, &CChildView::OnUpdateShapeTriangle)
+	ON_UPDATE_COMMAND_UI(ID_SHAPE_SQUARE, &CChildView::OnUpdateShapeSquare)
+END_MESSAGE_MAP()
+
+
+
+// CChildView 消息处理程序
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 用于绘制的设备上下文
+	
+	// TODO: 在此处添加消息处理程序代码
+	
+	// 不要为绘制消息而调用 CWnd::OnPaint()
+	CPoint points[3];
+
+	switch (m_nShape) {
+	case 0:
+		dc.Ellipse(100, 100, 200, 200);
+		break;
+	case 1:
+		points[0].x = 150;
+		points[0].y = 100;
+		points[1].x = 100;
+		points[1].y = 200;
+		points[2].x = 200;
+		points[2].y = 200;
+		dc.Polygon(points, 3);
+		break;
+	case 2:
+		dc.Rectangle(100, 100, 200, 200);
+		break;
+	}
+}
+
+
+
+void CChildView::OnShapeCircle()
+{
+	// TODO: 在此添加命令处理程序代码
+	m_nShape = 0;
+	Invalidate();
+}
+
+
+void CChildView::OnShapeTriangle()
+{
+	// TODO: 在此添加命令处理程序代码
+	m_nShape = 1;
+	Invalidate();
+}
+
+
+void CChildView::OnShapeSquare()
+{
+	// TODO: 在此添加命令处理程序代码
+	m_nShape = 2;
+	Invalidate();
+}
+
+
+void CChildView::OnUpdateShapeCircle(CCmdUI *pCmdUI)
+{
+	// TODO: 在此添加命令更新用户界面处理程序代码
+	pCmdUI->SetCheck(m_nShape == 0);
+}
+
+
+void CChildView::OnUpdateShapeTriangle(CCmdUI *pCmdUI)
+{
+	// TODO: 在此添加命令更新用户界面处理程序代码
+	pCmdUI->SetCheck(m_nShape == 1);
+}
+
+
+void CChildView::OnUpdateShapeSquare(CCmdUI *pCmdUI)
+{
+	// TODO: 在此添加命令更新用户界面处理程序代码
+	pCmdUI->SetCheck(m_nShape == 2);
+}
---
+++ 8 Menu/ChildView.h
@@ -0,0 +1,46 @@
+﻿
+// ChildView.h: CChildView 类的接口
+//
+
+
+#pragma once
+
+
+// CChildView 窗口
+
+class CChildView : public CWnd
+{
+// 构造
+public:
+	CChildView();
+
+// 特性
+public:
+
+// 操作
+public:
+
+// 重写
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// 实现
+public:
+	virtual ~CChildView();
+
+	// 生成的消息映射函数
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnShapeCircle();
+	afx_msg void OnShapeTriangle();
+	afx_msg void OnShapeSquare();
+protected:
+	int m_nShape;
+public:
+	afx_msg void OnUpdateShapeCircle(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateShapeTriangle(CCmdUI *pCmdUI);
+	afx_msg void OnUpdateShapeSquare(CCmdUI *pCmdUI);
+};
+
